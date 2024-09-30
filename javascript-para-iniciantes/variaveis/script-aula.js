11f762fff870b3c4db44650aeda49d717164db0b
rodrigosilva <rodrigonew290894@gmail.com>
2024-09-30 19:58:36 -0300

    variaveis

---
+++ javascript-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,38 @@
+var nome = 'André';
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael', cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+
+
+
+
+
+
+
+var time = 'Vasco';
+var time = 'Flamengo';
+
+console.log(time);
+
---
+++ javascript-para-iniciantes/variaveis/script-exercicios.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = 'Rodrigo';
+
+// Declarar uma variável com a sua idade
+var idade = 30;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Lasanha';
+
+// Declarar 5 variáveis diferentes sem valores
+var país;
+var estado;
+var cidade;
+var bairro;
+var rua;
+
