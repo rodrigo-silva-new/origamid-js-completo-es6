40addd7cab122fe5995e14baa2f242aa5b81f508
rodrigosilva <rodrigonew290894@gmail.com>
2024-10-05 15:34:58 -0300

    numeros e operadores

---
+++ 00-javascript-para-iniciantes/02-numeros-e-operadores/script-aula.js
@@ -0,0 +1,33 @@
+var total = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 345582435848 % 3;
+
+console.log(total);
+console.log(divisao);
+console.log(modulo);
+
+var testeNaN = 'Isso é 100' / 2;
+console.log(isNaN(testeNaN));
+
+
+
+
+
+
+
+
+var soma1 = 10 + 10 + 20 + 30 *4 / 2 + 10;
+console.log(soma1);
+
+var x = 5;
+console.log(x++);
+console.log(x++);
+console.log(x--);
+console.log(x--);
+
+
+var idade = '28';
+var somaIdade = 5;
+
+console.log(idade + somaIdade);
+console.log(+idade + somaIdade);
---
+++ 00-javascript-para-iniciantes/02-numeros-e-operadores/script-exercicios.js
@@ -0,0 +1,21 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+var resposta = '35';
+
+// Crie duas expressões que retornem NaN
+console.log('teste um' * 2);
+console.log(45 / 'b');
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero1 = 5;
+console.log(++numero1);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(peso);
