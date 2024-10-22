7fbff339c8b2b018b8cd73f46348b9f60e18713c
rodrigosilva <rodrigonew290894@gmail.com>
2024-10-22 19:29:36 -0300

    atribuicao e ternario

---
+++ 00-javascript-para-iniciantes/08-atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,37 @@
+/*
+var numero = 20;
+
+numero += 10;
+console.log(numero);
+*/
+
+
+
+
+
+
+
+var idade = 20;
+
+var podeBeber;
+podeBeber = (idade >= 18) ? true : false; // não faz sentido, pois retornará um booleano, mas a expressão já retornaria um booleano
+
+console.log(podeBeber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+var possuiFaculdade = false;
+if (possuiFaculdade)
+  console.log('Sim possui');
+else
+  console.log('Não possui');
