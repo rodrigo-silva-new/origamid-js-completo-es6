c4ee80afa661170db3665f2522bcab19167cab0f
rodrigosilva <rodrigonew290894@gmail.com>
2024-10-28 17:29:45 -0300

    escopo

---
+++ 00-javascript-para-iniciantes/09-escopo/script-aula.js
@@ -0,0 +1,77 @@
+/*
+function mostrarCarro() {
+  var carro = 'Fusca';
+  console.log(carro);
+}
+
+mostrarCarro(); // Fusca no console
+console.log(carro); // Erro, carro is not defined
+*/
+
+
+
+
+
+
+
+
+
+/*
+'use strict'
+var carro = 'Fusca';
+function mostrarCarro() {
+  console.log(carro);
+}
+
+mostrarCarro(); // Fusca no console
+console.log(carro); // Erro, carro is not defined
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+for (let i = 0; i < 10; i++) {
+  console.log(`Número ${i}`);
+}
+
+console.log(i);
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+const semana = 'Sexta';
+// semana = 'Quinta';
+// const semana = 'Quinta';
+console.log(semana);
+*/
+
+
+
+
+
+
+
+
+
+const data = {
+  ano: 2018,
+  mes: 'Dezembro',
+}
+
+data.ano = 2019;
