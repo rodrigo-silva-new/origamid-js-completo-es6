61e72f910254615a5d3baea4e9ee3a5e3229e6fc
Rodrigo Silva <142907639+rodrigo-silva-new@users.noreply.github.com>
2025-02-04 16:26:35 +0000

    constructor function 1

---
+++ 02-objetos/00-constructor-function-1/script-aula.js
@@ -0,0 +1,8 @@
+function Carro(marcaAtribuida, precoAtribuido) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
+
+const honda = new Carro("Honda", 3000);
+const fiat = new Carro("Fiat", 4000);
+fiat.marca = "Fiat";
