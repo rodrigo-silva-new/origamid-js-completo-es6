080e33d6c8d8fd40caf219e9701fc00079a7cd6b
Rodrigo Silva <142907639+rodrigo-silva-new@users.noreply.github.com>
2025-02-06 21:08:35 +0000

    prototype 2

---
+++ 02-objetos/03-prototype-2/exercicios.js
@@ -0,0 +1,31 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoas(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+Pessoas.prototype.nomeCompleto = function () {
+  return `${nome} ${this.sobrenome}`;
+};
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
