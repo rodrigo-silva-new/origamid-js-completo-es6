73155e28f7caf631a894f2175a417a473ffae28f
rodrigosilva <rodrigonew290894@gmail.com>
2025-02-07 19:08:15 -0300

    finaliza prototype

--- 02-objetos/02-prototype/script.js
+++ 02-objetos/02-prototype/script.js
@@ -15,3 +15,37 @@ const andre = new Pessoa("Andre", 28);
 
 console.log(Pessoa.prototype);
 console.log(andre.prototype);
+
+
+
+
+
+
+
+
+
+
+const pais = 'Brasil';
+const cidade = new String('Rio');
+
+const listaAnimais = ['Cachorro', 'Gato', 'Cavalo'];
+
+
+const lista = document.querySelectorAll('li');
+
+// Transforma em uma array
+const listaArray1 = Array.prototype.slice.call(lista);
+const listaArray2 = Array.from(lista);
+
+
+
+
+
+
+const Carro = {
+  marca: 'Ford',
+  preco: 2000,
+  andar() {
+    return 'Oi';
+  }
+}
