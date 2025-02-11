b9ee1cb31f2bf8315e28455057591bf04abaf896
Rodrigo Silva <142907639+rodrigo-silva-new@users.noreply.github.com>
2025-02-11 13:26:44 +0000

    string 2

--- 02-objetos/04-string/script.js
+++ 02-objetos/04-string/script.js
@@ -3,18 +3,18 @@
 
 // console.log(comida.length);
 
-const frase = "A melhor linguagem é ";
-const linguagem = "JavaScript";
+// const frase = "A melhor linguagem é ";
+// const linguagem = "JavaScript";
 
 // console.log(frase[0]);
 // console.log(frase[frase.length - 1]);
 // console.log(frase.charAt(frase.length - 1));
 
 // const fraseFinal = frase + linguagem;
-const fraseFinal = frase.concat(linguagem, "!!", "Oi asdkfasdf");
+// const fraseFinal = frase.concat(linguagem, "!!", "Oi asdkfasdf");
 
-const fruta = "Banana";
-const listaFrutas = "Melancia, Banana, Laranja";
+// const fruta = "Banana";
+// const listaFrutas = "Melancia, Banana, Laranja";
 
 // console.log(listaFrutas.includes(fruta));
 // console.log(listaFrutas.includes(fruta, 14));
@@ -22,9 +22,9 @@ const listaFrutas = "Melancia, Banana, Laranja";
 // console.log(fruta.startsWith("Ba"));
 // console.log(fruta.endsWith("na"));
 
-const transacao1 = "Depósito de cliente";
-const transacao2 = "Depósito de fornecedor";
-const transacao3 = "Taxa de camisas";
+// const transacao1 = "Depósito de cliente";
+// const transacao2 = "Depósito de fornecedor";
+// const transacao3 = "Taxa de camisas";
 
 // console.log(transacao1.slice(0, 3)); // Dep
 // console.log(transacao2.slice(0, 3)); // Dep
@@ -40,11 +40,44 @@ const transacao3 = "Taxa de camisas";
 
 // console.log(fruta.lastIndexOf("na"));
 
-const preco = "R$  99,00";
+// const preco = "R$  99,00";
 
-const listaPrecos = ["R$ 99", "R$ 199"];
-console.log()
+// const listaPrecos = ["R$ 99", "R$ 199"];
+// console.log()
 
-listaPrecos.forEach(item) => {
-  console.log(item.padStart(10));
-}
+// listaPrecos.forEach(item) => {
+//   console.log(item.padStart(10));
+// }
+
+const frase2 = "Ta";
+
+frase2.repeat(5);
+
+let listaItens = "Camisas Bonés Calças Bermudas Vestidos Sias";
+listaItens = listaItens.replace(/[ ]+/g, ", "); // Regular Expression
+// listaItens = listaItens.replace(" ", ", "); // sem regular expressoin, só aplica no primeiro
+
+const arrayLista = listaItens.split(", ");
+
+console.log(arrayLista);
+
+const htmlText = "<div>O melhor item </div><div>A melhor lista</div>";
+const htmlArray = htmlText.split("div");
+const novoHtml = htmlArray.join("ssection");
+
+console.log(htmlText);
+console.log(htmlArray);
+console.log(novoHtml);
+
+const frutasArray = ["Banana", "Melancia", "Laranja"];
+
+const sexo1 = "Feminino";
+const sexo2 = "feminino";
+const sexo3 = "FEMININO";
+
+console.log(sexo1.toLowerCase() === "feminino");
+
+const valor = " R$ 23,00 ";
+valor.trim();
+valor.trimStart();
+valor.trimEnd();
