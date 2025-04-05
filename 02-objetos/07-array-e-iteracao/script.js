bde9e4a1a5b58040615a6cb506fe9a2504db0f93
Rodrigo Silva <rodrigonew290894@gmail.com>
2025-04-05 11:06:02 -0300

    array e iteracao 2

--- 02-objetos/07-array-e-iteracao/script.js
+++ 02-objetos/07-array-e-iteracao/script.js
@@ -1,3 +1,7 @@
+/* ==================================================
+.forEach()
+================================================== */
+
 // const carros = ['Ford', 'Fiat', 'Honda'];
 
 // carros.forEach((item, index, array) => {
@@ -37,7 +41,9 @@
 
 
 
-
+/* ==================================================
+.map()
+================================================== */
 // O .map() sempre retorna uma nova Array:
 // const carros = ['Ford', 'Fiat', 'Honda'];
 
@@ -109,7 +115,9 @@
 
 
 
-
+/* ==================================================
+.reduce()
+================================================== */
 // .reduce() diferente do .map() retorna um valor único:
 // const aulas = [10, 25, 30];
 
@@ -124,13 +132,162 @@
 
 
 
-const numeros = [10, 25, 30, 3, 54, 33, 22];
+// const numeros = [10, 25, 30, 3, 54, 33, 22];
 
-const maiorNumero = numeros.reduce((anterior, atual) => {
-  if(anterior > atual)
-    return anterior
-  else
-    return atual
-}, 0)
+// const maiorNumero = numeros.reduce((anterior, atual) => {
+//   if(anterior > atual)
+//     return anterior
+//   else
+//     return atual
+// }, 0)
 
-console.log(maiorNumero);
+// console.log(maiorNumero);
+
+
+
+
+
+
+
+
+
+
+
+// const aulas = [
+//   {
+//     nome: 'HTML 1',
+//     min: 15
+//   },
+//   {
+//     nome: 'HTML 2',
+//     min: 10
+//   },
+//   {
+//     nome: 'CSS 1',
+//     min: 20
+//   },
+//   {
+//     nome: 'JS 1',
+//     min: 25
+//   },
+// ]
+
+// const listaAulas = aulas.reduce((acumulador, aula, index) => {
+//   console.log(aula.nome);
+//   acumulador[index] = aula.nome;
+//   return acumulador;
+// })
+
+
+
+// Passo a passo do .reduce():
+// 1º
+// aulas.reduce(({}, {nome: 'HTML 1', min: 15}, 0) => {
+//   {}[0] = 'HTML 1';
+//   return {0: 'HTML 1'};
+// }, {});
+
+// // 2º
+// aulas.reduce(({0: 'HTML 1'}, {nome: 'HTML 2', min: 10}, 1) => {
+//   {0: 'HTML 1'}[1] = 'HTML 2';
+//   return {0: 'HTML 1', 1: 'HTML 2'};
+// }, {});
+
+// // 3º
+// aulas.reduce(({0: 'HTML 1', 1: 'HTML 2'}, {nome: 'CSS 1', min: 20}, 2) => {
+//   {0: 'HTML 1', 1: 'HTML 2'}[2] = 'CSS 1';
+//   return {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'};
+// }, {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==================================================
+.reduceRight()
+================================================== */
+// Faz o mesmo que o reduce, mas pegando o array da direita pra esquerda;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==================================================
+.some() // .every()
+================================================== */
+// .some() = Se pelo menos um return da iteração for truthy, ele retorna true;
+// .evvery() = se todos os returns das iterações forem truthy, o método irá retornar true. Se pelo menos um for falsy, ele irá retornar false;
+
+const frutas = ['Banana', 'Pêra', 'Uva'];
+
+const temUva = frutas.some((item) => {
+  return item === 'Uva';
+})
+
+const every = frutas.every((item) => {
+  return item === 'Uva';
+})
+
+// console.log(temUva);
+console.log(every);
+
+
+
+
+
+const numeros = [6, 43, 22, 88, 101, 29];
+
+const maiorQue3 = numeros.every(n => n > 3);
+
+console.log(maiorQue3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==================================================
+
+================================================== */
+
