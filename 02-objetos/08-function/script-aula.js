70e4edc20668b360759f5fbc1f8e237cc484c510
Rodrigo Silva <rodrigonew290894@gmail.com>
2025-04-08 18:24:43 -0300

    efeitos no dom - function 2 incompleto

--- 02-objetos/08-function/script-aula.js
+++ 02-objetos/08-function/script-aula.js
@@ -84,25 +84,108 @@ function.call()
 
 
 
-const frutas = ['Uva', 'Maçã', 'Banana'];
+// const frutas = ['Uva', 'Maçã', 'Banana'];
 
-Array.prototype.pop.call(frutas);
-frutas.pop();
+// Array.prototype.pop.call(frutas);
+// frutas.pop();
 
 
 
-const arrayLike = {
-  0: 'Item 1',
-  1: 'Item 2',
-  2: 'Item 3',
-  length: 3,
-}
+// const arrayLike = {
+//   0: 'Item 1',
+//   1: 'Item 2',
+//   2: 'Item 3',
+//   length: 3,
+// }
 
-const li = document.querySelectorAll('li');
+// const li = document.querySelectorAll('li');
 
-const filtro = Array.prototype.filter.call(li, (item) => {
-  console.log(item);
-});
+// const filtro = Array.prototype.filter.call(li, (item) => {
+//   console.log(item);
+// });
 
-console.log(filtro);
-console.log(li);
+// console.log(filtro);
+// console.log(li);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==========================================================
+function.apply()
+========================================================== */
+// O apply(this, [arg1, arg2, ...]) funciona como o call, a única diferença é que 
+// os argumentos da função são passados através de uma array.
+
+// const numeros = [3, 4, 6, 1, 34, 44, 32];
+// Math.max.apply(null, numeros);
+// Math.max.call(null, 3, 4, 5, 6, 7, 20);
+
+// Podemos passar null para o valor 
+// de this, caso a função não utilize 
+// o objeto principal para funcionar
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==========================================================
+Apply vs Call
+========================================================== */
+// A única diferença é a array como segundo argumento.
+
+// const li = document.querySelector('li');
+
+// function itemPossuiAtivo(item) {
+//   return item.classList.contains('ativo');
+// }
+
+// const filtro1 = Array.prototype.filter.call(li, itemPossuiAtivo);
+// const filtro2 = Array.prototype.filter.apply(li, [itemPossuiAtivo]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==========================================================
+Apply vs Call
+========================================================== */
